88af06eba20d7e36d3e557c97dbf15ceb81dd53d
codebynikhil-cpp <nikhilkumar.ise27@gmail.com>
2025-06-28 19:58:50 +0000

    Arrays

---
+++ 02-basics/01_Array.js
@@ -0,0 +1,39 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+// const myHeors = ["shaktiman", "naagraj"]
+
+// const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9) //inserting in starting of the array
+// myArr.shift() // Deleting the first shifted number
+
+// console.log(myArr.includes(9)); //Boolean it show it's present or not
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join() //add all the element into the string (format / type change )
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1, 3) //incude 1 and 2 not 3
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+
+// const myn2 = myArr.splice(1, 3) //it change the original array also and include last element too
+// console.log("C ", myArr);
+// console.log(myn2);
