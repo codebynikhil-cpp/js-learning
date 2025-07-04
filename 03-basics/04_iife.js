25d387ee06fc1050fc8b6200c11a230dcf7b2965
codebynikhil-cpp <nikhilkumar.ise27@gmail.com>
2025-07-04 17:33:03 +0000

    iife

--- 03-basics/04_iife.js
+++ 03-basics/04_iife.js
@@ -12,4 +12,33 @@
 ( (name) => {
             console.log(`DB CONNECTED TO ${name}`);
             } )('NIkhil')
+
+
+            //JavaScript Execution Context
+//Global Excution code { } <- this
+            // Global Execution Context
+            // Function Execution Context
+            // Eval Execution Context
+
+            // {} <- javascript Code
+                //Memory Creation Phase
+                // Execution Phase
+
+
+Running a JavaScript Code Step by Step
+1> Global Execution -> this
+2> Memory Phase -> 
+            val1->undefined 
+            val2 -> undefined 
+            addnum -> definition
+            result1 -> undefined
+            result2 -> undefined
+3> Execution Phase 
+            val1 = 10
+            val2 = 5
+            addNum = [new variable environment + Execution thread]
+
+            
+
+
             
