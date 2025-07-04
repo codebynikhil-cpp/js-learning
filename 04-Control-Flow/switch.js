961f942a64e3cd2b89b5349c25d94c180cbb8d0d
codebynikhil-cpp <nikhilkumar.ise27@gmail.com>
2025-07-04 21:04:48 +0000

    Switch Statement

---
+++ 04-Control-Flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// shift + alt + down arrow for copying down
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
